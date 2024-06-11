93169088cb98cfdba4a8cfc3accfe65c8449c4f8
Sofia Buitrago <laurabuca98@gmail.com>
2024-06-11 12:09:16 -0600

    install commands

--- db/connect.js
+++ db/connect.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 
 
+
+
 const username = "laurabuitrago98";
-const password = "2011458Sofia";
+const password = "1234";
 const dbname = "music_app"; //Create a data base
 
-const uri = `mongodb+srv://laurabuitrago98:${password}@cluster0.lofhwso.mongodb.net/${dbname}?retryWrites=true&w=majority`;;
+const uri = `mongodb+srv://laurabuitrago98:${password}@cluster0.lofhwso.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 mongoose.connect(uri).then((con)=>{
     console.log("connected to MongoDB");
 }).catch((err)=>{
@@ -15,3 +17,14 @@ mongoose.connect(uri).then((con)=>{
 });
 
 const db = mongoose.connection;
+
+
+// // user creds for db
+// const username = "laurabuitrago98";
+// const passwd = "1234";
+
+
+// // database details
+// const dbname = "game";
+// //const uri = mongodb+srv://${username}:${passwd}@web602-db.gr9k9w0.mongodb.net/${dbname}?retryWrites=true&w=majority&appName=WEB602-DB;
+// const uri = `mongodb+srv://laurabuitrago98:${passwd}@cluster0.lofhwso.mongodb.net/${dbname}?retryWrites=true&w=majority`;
